4890706895b38db3d09a6a3bf4408f477b5395c8
bo545008 <mrbolar545008@gmail.com>
2024-12-04 13:57:39 +0900

    Hi guys

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onomatope</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="menu-wrap">
+            <a class="box1 box" id="random" href="#">
+                <h2>ランダムで勉強する</h2>
+            </a>
+
+            <a class="box2 box" id="category"  href="#">
+                <h2>カテゴリーで勉強する</h2>
+            </a>
+        </div>
+    
+        <div class="second">
+            <a class="box3 box" id="liked" href="#">
+                <h2>お気に入りを見る</h2>
+            </a>
+
+            <a class="box4 box" id="history" href="#">
+                <h2>履歴を見る</h2>
+            </a>
+
+
+        </div>
+    </div>
+</body>
+
+</html>
