00e4a0534c10728e79688ec905b9f6338fbfc698
OKAOKA8 <roa777893@gmail.com>
2024-12-04 14:40:35 +0900

    update db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$dns = 'mysql:host=localhost; dbname=onomatope_db;';
+$user = 'kaito';
+$password = 'password';
+$conn = null;
+
+try {
+    $conn = new PDO($dns, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
