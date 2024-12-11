d930aa031a3d3f490b1deb51c77999cae47ff94a
seinnlet <sllhninn.6@gmail.com>
2024-12-11 11:09:23 +0900

    update main menu design

--- index.php
+++ index.php
@@ -4,35 +4,29 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Onomatope</title>
-	<link rel="stylesheet" href="css/style.css">
+	<title>ペラペラオノマトペ</title>
+	<!-- fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700;800;900&display=swap"
+		rel="stylesheet">
+	<!-- css -->
+	<link rel="stylesheet" href="style.css">
 </head>
 
-<body>
-	<div class="container">
-
-		<div class="menu-wrap">
-			<a class="box1 box" id="random" href="#">
-				<h2>ランダムで勉強する</h2>
-			</a>
-
-			<a class="box2 box" id="category" href="#">
-				<h2>カテゴリーで勉強する</h2>
-			</a>
+<body class="flex-box">
+	<main class="flex-box">
+		<div class="wrapper flex-box">
+			<div class="main-title">ペラペラオノマトペ</div>
+			<div class="main-links">
+				<a href="#" class="btn btn-main-menu">わくわくコース</a>
+			</div>
+			<div class="sub-links">
+				<a href="#" class="btn btn-sub-menu">ドキドキことば</a>
+				<a href="#" class="btn btn-sub-menu">コツコツストーリー</a>
+			</div>
 		</div>
-
-		<div class="second">
-			<a class="box3 box" id="liked" href="#">
-				<h2>お気に入りを見る</h2>
-			</a>
-
-			<a class="box4 box" id="history" href="#">
-				<h2>履歴を見る</h2>
-			</a>
-
-
-		</div>
-	</div>
+	</main>
 </body>
 
 </html>
